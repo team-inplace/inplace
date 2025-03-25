ef0725a6dea3b7dcbe295adeb089905b9326a620
sanghee0820 <sanghee0820.dev@gmail.com>
2025-03-25 21:42:48 +0900

    [INPLACE-188] AutoComplete에 type 파라미터 추가

--- backend/src/main/java/team7/inplace/search/application/dto/SearchType.java
+++ backend/src/main/java/team7/inplace/search/application/dto/SearchType.java
@@ -1,5 +1,17 @@
 package team7.inplace.search.application.dto;
 
 public enum SearchType {
-    PLACE, INFLUENCER, VIDEO
+    PLACE, INFLUENCER, VIDEO, ALL;
+
+    public static SearchType from(String type) {
+        if (type == null || type.isBlank()) {
+            return ALL;
+        }
+
+        if (!type.equals("all") && !type.equals("place")) {
+            throw new IllegalArgumentException("Invalid type");
+        }
+
+        return SearchType.valueOf(type.toUpperCase());
+    }
 }
