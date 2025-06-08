8ca33a0092330559a282c69dcb7e3c2e54294ae5
Suhyeon7497 <nucc1186@gmail.com>
2025-06-08 16:25:04 +0900

    [INPLACE-248] userTitle -> Badge로 이름 변경

--- backend/src/main/java/team7/inplace/user/domain/Badge.java
+++ backend/src/main/java/team7/inplace/user/domain/Badge.java
@@ -5,23 +5,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(name = "badgess")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserTitle {
+public class Badge {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "title", length = 10)
-    private String title;
+    @Column(name = "name", length = 10)
+    private String name;
 
     @Column(name = "condition", length = 30)
     private String condition;
 
+    @Column(name = "img_url")
+    private String imgUrl;
 }
