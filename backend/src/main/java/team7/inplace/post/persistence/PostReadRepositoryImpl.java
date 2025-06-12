0e7624c8c113bd86a5b6dda3d74d705fc18a80d8
Suhyeon7497 <nucc1186@gmail.com>
2025-06-12 23:49:32 +0900

    [INPLACE-248] 오류 수정

--- backend/src/main/java/team7/inplace/post/persistence/PostReadRepositoryImpl.java
+++ backend/src/main/java/team7/inplace/post/persistence/PostReadRepositoryImpl.java
@@ -91,7 +91,7 @@ public class PostReadRepositoryImpl implements PostReadRepository {
             .from(QPost.post)
             .innerJoin(QUser.user).on(QPost.post.authorId.eq(QUser.user.id))
             .innerJoin(QUserTier.userTier).on(QUser.user.tierId.eq(QUserTier.userTier.id))
-            .leftJoin(QUserBadge.userBadge).on(QUser.user.mainBadgeId.eq(QBadge.badge.id))
+            .leftJoin(QBadge.badge).on(QUser.user.mainBadgeId.eq(QBadge.badge.id))
             .leftJoin(QLikedPost.likedPost).on(likedJoinCondition);
     }
 
@@ -121,7 +121,7 @@ public class PostReadRepositoryImpl implements PostReadRepository {
             .from(QPost.post)
             .innerJoin(QUser.user).on(QPost.post.authorId.eq(QUser.user.id))
             .innerJoin(QUserTier.userTier).on(QUser.user.tierId.eq(QUserTier.userTier.id))
-            .leftJoin(QUserBadge.userBadge).on(QUser.user.mainBadgeId.eq(QBadge.badge.id))
+            .leftJoin(QBadge.badge).on(QUser.user.mainBadgeId.eq(QBadge.badge.id))
             .leftJoin(QLikedPost.likedPost).on(likedJoinCondition)
             .where(QPost.post.id.eq(postId));
     }
--- backend/src/test/java/team7/inplace/influencer/persistence/InfluencerReadRepositoryImplTest.java
+++ backend/src/test/java/team7/inplace/influencer/persistence/InfluencerReadRepositoryImplTest.java
@@ -2,6 +2,7 @@ package team7.inplace.influencer.persistence;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,12 +11,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import team7.inplace.influencer.persistence.dto.InfluencerQueryResult;
 
 @DataJpaTest
+@Import(ObjectMapper.class)
 @ActiveProfiles("test")
 @Sql("/sql/test-influencer.sql")
 class InfluencerReadRepositoryImplTest {
