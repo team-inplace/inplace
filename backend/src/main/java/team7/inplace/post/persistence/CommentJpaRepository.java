99641614faf16104409b58374bfd686e34882d9d
hyunseo <wndlthsk@naver.com>
2025-06-14 23:58:50 +0900

    [INPLACE-253] 신고 요청시 판단 후 소프트딜리트 처리 구현

--- backend/src/main/java/team7/inplace/post/persistence/CommentJpaRepository.java
+++ backend/src/main/java/team7/inplace/post/persistence/CommentJpaRepository.java
@@ -1,10 +1,16 @@
 package team7.inplace.post.persistence;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import team7.inplace.post.domain.Comment;
 
 @Repository
 public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT c.content FROM Comment c WHERE c.id = :commentId")
+    Optional<String> findContentById(@Param("commentId") Long commentId);
 
 }
