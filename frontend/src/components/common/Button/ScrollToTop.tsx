783ce3c62e0e8c4c2f34d84eedcfef1ace64e0c7
Hyoeunkh <lhe4528@knu.ac.kr>
2025-06-30 17:23:09 +0900

    [INPLACE-249] 스크롤 시 임계값 이상일 경우에 상단으로 이동 버튼 보이기

--- frontend/src/components/common/Button/ScrollToTop.tsx
+++ frontend/src/components/common/Button/ScrollToTop.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { IoIosArrowUp } from 'react-icons/io';
+import { useEffect, useState } from 'react';
 import useScrollToTop from '@/hooks/useScrollToTop';
 import Button from '.';
 import useTheme from '@/hooks/useTheme';
@@ -8,6 +9,24 @@ export default function ScrollToTop() {
   const { theme } = useTheme();
   const isDark = theme === 'dark';
   const handleClick = useScrollToTop();
+
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  if (!show) return null;
+
   return (
     <ButtonWrapper aria-label="상단 이동 버튼" variant={isDark ? 'outline' : 'blackOutline'} onClick={handleClick}>
       <IoIosArrowUp size={30} />
