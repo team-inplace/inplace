584e24bb5338bf2f9aabe254b4df849c882a4678
Hyoeunkh <lhe4528@knu.ac.kr>
2025-07-13 20:46:25 +0900

    [INPLACE-261] 웹뷰 연결

--- frontend/mobile/App.tsx
+++ frontend/mobile/App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Platform, SafeAreaView, StyleSheet } from "react-native";
+import WebView from "react-native-webview";
 
 export default function App() {
+  const webViewUrl = "https://www.inplace.my";
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: webViewUrl }}
+        allowsbackforwardnavigationgestures={true}
+        style={styles.webview}
+      />
+    </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // iOS에서 Safe Area를 처리 (노치 디자인 고려)
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+  webview: {
+    flex: 1,
   },
 });
