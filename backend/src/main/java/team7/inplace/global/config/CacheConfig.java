7725fd7db95acb49bdef13272ad8081b5ba56b6e
Suhyeon7497 <nucc1186@gmail.com>
2025-07-21 23:16:30 +0900

    [INPLACE-248] @enableCaching to CacheConfig

--- backend/src/main/java/team7/inplace/global/config/CacheConfig.java
+++ backend/src/main/java/team7/inplace/global/config/CacheConfig.java
@@ -4,10 +4,12 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableCaching
 @Configuration
 public class CacheConfig {
 
