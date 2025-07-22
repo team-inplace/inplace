307ac13a9f2b7a409c614774080c4939f0723b31
Suhyeon7497 <nucc1186@gmail.com>
2025-07-22 22:07:42 +0900

    [INPLACE-248] test 깨지는 거 수정

--- backend/src/test/resources/sql/test-user.sql
+++ backend/src/test/resources/sql/test-user.sql
@@ -4,10 +4,10 @@ INSERT INTO user_tiers (name, eng_name, required_posts, required_comments, requi
                                                                                                             ( '실버', 'SILVER', 10, 5, 20, 'silver.png');
 
 -- 2. users
-INSERT INTO users (created_at, delete_at, update_at, nickname, password, username, role, user_type, profile_image_url, tier_id, main_badge_id, post_count, received_comment_count, received_like_count) VALUES
-                                                                                                                                        (NOW(), NULL, NOW(), '유저1', 'pass1', 'user1@gmail.com', 'FIRST_USER', 'KAKAO', 'img1.png', 1, 1, 100, 2L, 10L),
-                                                                                                                                        (NOW(), NULL, NOW(), '유저2', 'pass2', 'user2@gmail.com', 'FIRST_USER', 'KAKAO', 'img2.png', 2, 2, 1000, 10L, 110001L),
-                                                                                                                                        (NOW(), NULL, NOW(), '유저3', 'pass3', 'user3@gmail.com', 'ADMIN', 'KAKAO', 'img3.png', 2, 3, 101, 123L, 1230L);
+INSERT INTO users (id, created_at, delete_at, update_at, nickname, password, username, role, user_type, profile_image_url, tier_id, main_badge_id, post_count, received_comment_count, received_like_count) VALUES
+                                                                                                                                        (1, NOW(), NULL, NOW(), '유저1', 'pass1', 'user1@gmail.com', 'FIRST_USER', 'KAKAO', 'img1.png', 1, 1, 100, 2L, 10L),
+                                                                                                                                        (2, NOW(), NULL, NOW(), '유저2', 'pass2', 'user2@gmail.com', 'FIRST_USER', 'KAKAO', 'img2.png', 2, 2, 1000, 10L, 110001L),
+                                                                                                                                        (3, NOW(), NULL, NOW(), '유저3', 'pass3', 'user3@gmail.com', 'ADMIN', 'KAKAO', 'img3.png', 2, 3, 101, 123L, 1230L);
 
 -- 3. badges
 INSERT INTO badges (conditions, name, img_url) VALUES
