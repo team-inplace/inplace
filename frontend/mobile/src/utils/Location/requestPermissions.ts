fafe97ffc7b76a436e84b49bc56d73645b85a787
Hyoeunkh <lhe4528@knu.ac.kr>
2025-09-12 15:07:52 +0900

    [INPLACE-290] 주석 제거

--- frontend/mobile/src/utils/Location/requestPermissions.ts
+++ frontend/mobile/src/utils/Location/requestPermissions.ts
@@ -2,7 +2,6 @@ import * as Location from "expo-location";
 
 const requestPermissions = async (): Promise<boolean> => {
   const { status } = await Location.requestForegroundPermissionsAsync();
-  console.log("Location Permission Status:", status);
   return status === "granted";
 };
 
