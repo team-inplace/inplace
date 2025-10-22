837ce483bfaaa3e43a8cc8ee2a3bdbe7cbd6f00e
BaeJunH0 <bjh14757@naver.com>
2025-10-22 19:15:55 +0900

    [INPLACE-307] 사용하지 않는 필터 삭제

--- backend/inplace-security/src/main/java/my/inplace/security/config/SecurityFilterConfig.java
+++ backend/inplace-security/src/main/java/my/inplace/security/config/SecurityFilterConfig.java
@@ -1,10 +1,8 @@
 package my.inplace.security.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import my.inplace.security.filter.ApiExecutionMetricsFilter;
 import my.inplace.security.filter.AuthorizationFilter;
 import my.inplace.security.filter.ExceptionHandlingFilter;
 import my.inplace.security.util.JwtUtil;
@@ -20,12 +18,5 @@ public class SecurityFilterConfig {
     @Bean
     public ExceptionHandlingFilter exceptionHandlingFilter(ObjectMapper objectMapper) {
         return new ExceptionHandlingFilter(objectMapper);
-    }
-
-    @Bean
-    public ApiExecutionMetricsFilter apiExecutionMetricsFilter(
-        MeterRegistry meterRegistry, ObjectMapper objectMapper
-    ) {
-        return new ApiExecutionMetricsFilter(meterRegistry, objectMapper);
     }
 }
