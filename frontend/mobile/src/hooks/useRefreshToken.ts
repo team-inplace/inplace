5b8a9035cc0be8aeeae6360b73088950e5d119e7
Hyoeunkh <lhe4528@knu.ac.kr>
2025-11-03 21:57:14 +0900

    [INPLACE-316] 불필요한 코드 제거

--- frontend/mobile/src/hooks/useRefreshToken.ts
+++ frontend/mobile/src/hooks/useRefreshToken.ts
@@ -1,5 +1,3 @@
-import { login } from "@react-native-kakao/user";
-import { getAccessToken } from "../api/getAccessToken";
 import * as SecureStore from "expo-secure-store";
 import WebView from "react-native-webview";
 
