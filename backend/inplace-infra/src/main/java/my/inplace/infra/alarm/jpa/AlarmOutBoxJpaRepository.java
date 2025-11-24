dfd129d583aa792c24deb8bf6d04db134497ded4
BaeJunH0 <bjh14757@naver.com>
2025-11-25 01:34:58 +0900

    [INPLACE-319] 로그인 순간 로직 구현

--- backend/inplace-infra/src/main/java/my/inplace/infra/alarm/jpa/AlarmOutBoxJpaRepository.java
+++ backend/inplace-infra/src/main/java/my/inplace/infra/alarm/jpa/AlarmOutBoxJpaRepository.java
@@ -5,8 +5,9 @@ import my.inplace.domain.alarm.AlarmStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.UUID;
 
-public interface AlarmOutBoxRepository extends JpaRepository<AlarmOutBox, Long> {
+public interface AlarmOutBoxJpaRepository extends JpaRepository<AlarmOutBox, Long> {
     List<AlarmOutBox> findAllByAlarmStatus(AlarmStatus alarmStatus);
+    
+    List<AlarmOutBox> findAllByReceiverId(Long receiverId);
 }
