33de8c1be727784bf66797a8f1d85c9be7ec812d
BaeJunH0 <bjh14757@naver.com>
2025-11-25 01:34:58 +0900

    [INPLACE-319] 로그인 순간 로직 구현

--- backend/inplace-application/src/main/java/my/inplace/application/alarm/event/AlarmEventHandler.java
+++ backend/inplace-application/src/main/java/my/inplace/application/alarm/event/AlarmEventHandler.java
@@ -1,35 +1,24 @@
 package my.inplace.application.alarm.event;
 
 import my.inplace.application.alarm.event.dto.AlarmEvent;
-import my.inplace.application.post.query.dto.CommentResult;
 import my.inplace.domain.alarm.AlarmOutBox;
-import my.inplace.domain.alarm.AlarmType;
 import my.inplace.infra.alarm.ExpoClient;
 import my.inplace.infra.alarm.FcmClient;
-import my.inplace.application.alarm.command.AlarmCommandService;
-import com.google.firebase.messaging.FirebaseMessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import my.inplace.infra.alarm.jpa.AlarmOutBoxRepository;
+import my.inplace.infra.alarm.jpa.AlarmOutBoxJpaRepository;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
-import my.inplace.application.post.query.PostQueryService;
 import my.inplace.application.user.query.UserQueryService;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionalEventListener;
-import org.springframework.transaction.support.TransactionSynchronization;
-import org.springframework.transaction.support.TransactionSynchronizationAdapter;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
-
-import java.util.Optional;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class AlarmEventHandler {
     
-    private final AlarmOutBoxRepository alarmOutBoxRepository;
+    private final AlarmOutBoxJpaRepository alarmOutBoxJpaRepository;
     private final UserQueryService userQueryService;
     private final FcmClient fcmClient;
     private final ExpoClient expoClient;
@@ -38,7 +27,7 @@ public class AlarmEventHandler {
     @EventListener
     @Transactional
     public void processMentionAlarm(AlarmEvent alarmEvent) {
-        AlarmOutBox outBoxEvent = alarmOutBoxRepository.findById(alarmEvent.id())
+        AlarmOutBox outBoxEvent = alarmOutBoxJpaRepository.findById(alarmEvent.id())
             .orElseThrow();
         
         String fcmToken = userQueryService.getFcmTokenByUser(outBoxEvent.getReceiverId());
